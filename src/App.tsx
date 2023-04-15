97df11afb6e20a2cd1b3ad28edc4530e292bfb8a
Na Lin <lna483018@gmail.com>
2023-04-14 20:56:14 -0400

    integrate custom dark theme to react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React, { FC, ReactElement } from 'react';
 
+// MUI
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customDarkTheme } from './theme/customTheme';
+
 const App: FC = (): ReactElement => {
-  return <h1>Hello world</h1>;
+  return (
+    <ThemeProvider theme={customDarkTheme}>
+      <CssBaseline />
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  );
 };
 
 export default App;
