7f4afa4d7c9de2815787167d4bc44e9517ea10f6
Na Lin <lna483018@gmail.com>
2023-04-16 15:44:25 -0400

    Task Area layout

--- src/components/taskArea/TaskArea.tsx
+++ src/components/taskArea/TaskArea.tsx
@@ -1,12 +1,55 @@
 import React, { FC, ReactElement } from 'react';
 
 // MUI
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
+
+// Date format
+import { format } from 'date-fns';
 
 const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>Content area</h2>
+      <Box mb={8} px={4}>
+        <h2>
+          Status Of Your Tasks As On{' '}
+          {format(new Date(), 'PPPP')}
+        </h2>
+      </Box>
+
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+      >
+        {/* Task Counter */}
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <Box>Task Counter</Box>
+          <Box>Task Counter</Box>
+          <Box>Task Counter</Box>
+        </Grid>
+
+        {/* List of Task Content */}
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          md={8}
+          xs={10}
+        >
+          <Box>Task Counter</Box>
+          <Box>Task will come over here</Box>
+          <Box>Task will come over here</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
