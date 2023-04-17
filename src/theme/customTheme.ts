174903ffdda915929c1555c5806967f02a485e6e
Na Lin <lna483018@gmail.com>
2023-04-16 22:15:45 -0400

    update background color which I like

--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -2,15 +2,14 @@ import { createTheme, ThemeOptions } from '@mui/material';
 
 export const customDarkTheme: ThemeOptions = createTheme({
   palette: {
-    mode: 'dark',
     primary: {
       light: '#bbf7d0',
       main: '#22c55e',
       dark: '#15803d',
     },
     background: {
-      paper: '#334155',
-      default: '#0f172a',
+      paper: '#fde68a', // Amber 500
+      default: '#fef3c7', // Amber 200
     },
   },
 });
