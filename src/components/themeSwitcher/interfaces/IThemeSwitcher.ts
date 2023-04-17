0a5283e3b5c44c2f41ddd5d0ebb08cb07c90d681
Na Lin <lna483018@gmail.com>
2023-04-17 19:51:54 -0400

    use enum and union for set of constant value

--- src/components/themeSwitcher/interfaces/IThemeSwitcher.ts
+++ src/components/themeSwitcher/interfaces/IThemeSwitcher.ts
@@ -1,7 +1,14 @@
 import React from 'react';
 
+export enum Mode {
+  light = 'light',
+  dark = 'dark',
+}
+
+export type ThemeMode = Mode.light | Mode.dark;
+
 export interface IThemeSwitcher {
-  mode?: string;
+  mode?: ThemeMode;
   onChange?: (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => void;
