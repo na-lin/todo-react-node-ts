2aa97b9af07535640402176448220226e742677f
Na Lin <lna483018@gmail.com>
2023-04-16 22:18:25 -0400

    update profile name

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -25,7 +25,7 @@ const Sidebar: FC = (): ReactElement => {
         alignItems: 'center',
       }}
     >
-      <Profile name="Na" />
+      <Profile name="Jonas" />
       <CreateTaskForm />
     </Grid>
   );
